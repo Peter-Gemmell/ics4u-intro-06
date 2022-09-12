b842f0dca278bee8f43af1211bf7a2cc8f8f0402
Peter Gemmell <peter.gemmell@mths.ca~>
2022-09-12 21:45:27 +0000

    Linting about

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * The HelloWorld program implements an application that
+ * simply displays "Hello World!" to the standard output.
+ *
+ * @author  Peter Gemmell
+ * @version 1.0
+ * @since   2022-09-12
+ */
+
+console.log('Hello, World!')
+
+console.log('\nDone')
